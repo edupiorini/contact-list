86c9a1045e56ad23a722e059d89329c1ffbc77b5
edupiorini <eduardopiorini@hotmail.com>
2020-10-06 21:11:28 -0300

    fields now receive text data

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -13,6 +13,9 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
   bool _userEdited = false;
   Contact _editedContact;
 
@@ -24,6 +27,9 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 
@@ -57,6 +63,7 @@ class _ContactPageState extends State<ContactPage> {
               ),
             ),
             TextField(
+              controller: _nameController,
               decoration: InputDecoration(labelText: "Nome"),
               onChanged: (text) {
                 _userEdited = true;
@@ -66,6 +73,7 @@ class _ContactPageState extends State<ContactPage> {
               },
             ),
             TextField(
+              controller: _emailController,
               decoration: InputDecoration(labelText: "Email"),
               onChanged: (text) {
                 _userEdited = true;
@@ -74,6 +82,7 @@ class _ContactPageState extends State<ContactPage> {
               keyboardType: TextInputType.emailAddress,
             ),
             TextField(
+              controller: _phoneController,
               decoration: InputDecoration(labelText: "Phone"),
               onChanged: (text) {
                 _userEdited = true;
